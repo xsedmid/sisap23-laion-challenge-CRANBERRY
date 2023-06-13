c9c47b9c61577c39b96712ded2d1faef8e9eba36
xsedmid <sedmidubsky@gmail.com>
2023-06-13 15:50:51 +0200

    search - jar + pivots

--- search.py
+++ search.py
@@ -89,7 +89,8 @@ def run(root_data_folder, kind, key, size="100K", k=30):
     start = time.time()
     #subprocess.check_output(['java', '-cp', 'VMTrials', 'vm.vmtrials.tripleFiltering_Challenge.Main', root_data_folder, dataset_orig, dataset, query_orig, query], universal_newlines=True)
     #subprocess.check_output(['java', '-cp', os.path.join(os.getcwd(), 'VMTrials', 'target', 'classes'), 'vm.vmtrials.tripleFiltering_Challenge.Main', dataset_orig, dataset, query_orig, query, '1'], universal_newlines=True)
-    subprocess.check_output(['java', '-jar', os.path.join(os.getcwd(), 'VMTrials', 'target', 'VMTrials-1.0-SNAPSHOT-jar-with-dependencies.jar'), 'vm.vmtrials.tripleFiltering_Challenge.Main', dataset_orig, dataset, query_orig, query, '1'], universal_newlines=True)
+    #subprocess.check_output(['java', '-jar', os.path.join(os.getcwd(), 'VMTrials', 'target', 'VMTrials-1.0-SNAPSHOT-jar-with-dependencies.jar'), 'vm.vmtrials.tripleFiltering_Challenge.Main', dataset_orig, dataset, query_orig, query, '1'], universal_newlines=True)
+    subprocess.check_output(['java', '-jar', os.path.join(os.getcwd(), 'VMTrials', 'target', 'VMTrials-1.0-SNAPSHOT-jar-with-dependencies.jar'), dataset_orig, dataset, query_orig, query, '1'], universal_newlines=True)
 
     elapsed_build = time.time() - start
     print(f"*** Done in {elapsed_build}s.")
