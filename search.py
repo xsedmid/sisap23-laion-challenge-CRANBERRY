aa5a8bcaf191ff3442420b778fe1efe441146bfa
xsedmid <sedmidubsky@gmail.com>
2023-06-13 11:01:44 +0200

    search - test

--- search.py
+++ search.py
@@ -12,15 +12,21 @@ def download(src, dst):
         print('downloading %s -> %s...' % (src, dst))
         urlretrieve(src, dst)
 
-def prepare(kind, size):
+def prepare(root_data_folder, kind, size):
     url = "https://sisap-23-challenge.s3.amazonaws.com/SISAP23-Challenge"
     task = {
-        "query": f"{url}/public-queries-10k-{kind}.h5",
+        "dataset_orig": f"{url}/laion2B-en-clip768v2-n={size}.h5",
         "dataset": f"{url}/laion2B-en-{kind}-n={size}.h5",
+        "query_orig": f"{url}/public-queries-10k-clip768v2.h5",
+        "query": f"{url}/public-queries-10k-{kind}.h5"
     }
 
     for version, url in task.items():
-        download(url, os.path.join("data", kind, size, f"{version}.h5"))
+        result_file_path = os.path.join(root_data_folder, kind, size, f"{version}.h5")
+        if not os.path.exists(result_file_path):
+            download(url, result_file_path)
+    
+    return task["dataset_orig"], task["dataset"]
 
 def store_results(dst, algo, kind, D, I, buildtime, querytime, params, size):
     os.makedirs(Path(dst).parent, exist_ok=True)
@@ -35,22 +41,31 @@ def store_results(dst, algo, kind, D, I, buildtime, querytime, params, size):
     f.create_dataset('dists', D.shape, dtype=D.dtype)[:] = D
     f.close()
 
-def run(kind, key, size="100K", k=30):
+def run(root_data_folder, kind, key, size="100K", k=30):
     print("Running", kind)
     
-    prepare(kind, size)
+    dataset_orig, dataset = prepare(root_data_folder, kind, size)
 
-    data = np.array(h5py.File(os.path.join("data", kind, size, "dataset.h5"), "r")[key])
-    queries = np.array(h5py.File(os.path.join("data", kind, size, "query.h5"), "r")[key])
-    print(f'data.shape={data.shape}, queries.shape={queries.shape}')
+    #data = np.array(h5py.File(os.path.join("data", kind, size, "dataset.h5"), "r")[key])
+    #queries = np.array(h5py.File(os.path.join("data", kind, size, "query.h5"), "r")[key])
+    #print(f'data.shape={data.shape}, queries.shape={queries.shape}')
+
+    # root_folder-similarity_search; h5 path: data768, data96pca, query768, query96pca
 
     print(f"*** Running Java-based implementation (building the index + searching)...")
+    print(f"*** args:")
+    print(f"  {root_data_folder}")
+    print(f"  {dataset_orig}")
+    print(f"  {dataset}")
     start = time.time()
 
 
     elapsed_build = time.time() - start
     print(f"*** Done in {elapsed_build}s.")
     
+    # conversion of .csv results to .h5 format
+
+
 
     # for nprobe in [1, 2, 5, 10, 20, 50, 100]:
     #     print(f"Starting search on {queries.shape} with nprobe={nprobe}")
@@ -82,6 +97,5 @@ if __name__ == "__main__":
 
     assert args.size in ["100K", "300K", "10M", "30M", "100M"]
 
-    # run("pca32v2", "pca32", args.size, args.k)
-    run("pca96v2", "pca96", args.size, args.k)
-    # run("hammingv2", "hamming", args.size, args.k)
+    root_data_folder = "data"
+    run(root_data_folder, "pca96v2", "pca96", args.size, args.k)
