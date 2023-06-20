5a64535a410d17b19e56701d9647e1587824a97d
xsedmid <sedmidubsky@gmail.com>
2023-06-20 16:01:23 +0200

    test result + eval

--- search.py
+++ search.py
@@ -96,10 +96,18 @@ def run(root_data_folder, kind, key, size="100K", k=30):
         output_params = f.read()
         output_params = dict(item.split(":") for item in output_params.strip().split(";"))
         print(f"*** output_params: {output_params}")
+    algo = 'SimRelv1'
+    result_dst = os.path.join(result_dir, kind, size, f'{algo}.h5')
+    buildtime = output_params['buildtime']
+    querytime = output_params['querytime']
+    if 'params' in output_params:
+        params = output_params['params']
+    else:
+        params = ''
     
     # conversion of .csv results to .h5 format
-    #result_dir = os.path.join(root_data_folder, 'result')
     import shutil
+    print(f'Processing result file: {result_file_path}')
     algorithm_result_dir = os.path.join(root_data_folder, 'Result')
     result_dir = 'result'
     if not os.path.exists(result_dir):
@@ -109,23 +117,18 @@ def run(root_data_folder, kind, key, size="100K", k=30):
     # if not os.path.exists(result_file_path):
     #     download(f"https://www.fi.muni.cz/~xsedmid/temp/{result_file}", result_file_path)
     shutil.copyfile(os.path.join(algorithm_result_dir, result_file), result_file_path)
-    print(f'Processing result file: {result_file_path}')
 
     # Read result file with Pandas
     import pandas as pd
     df = pd.read_csv(result_file_path, header=None, skiprows=0, sep=';')
     # Print the number of rows and columns
     print(f'Test result file shape: {df.shape}')
-
-    algo = 'SimRelv1'
-    result_dst = os.path.join(result_dir, kind, size, f'{algo}.h5')
-    buildtime = output_params['buildtime']
-    querytime = output_params['querytime']
-    params = f'params of {algo}: []'
-
-    I = df.copy().applymap(lambda x: x.split(':')[0]).astype(int).to_numpy()
-    D = df.copy().applymap(lambda x: x.split(':')[1]).astype(float).to_numpy()
-    store_results(result_dst, algo, kind, D, I, buildtime, querytime, algo, size)
+    print(df)
+    # I = df.copy().applymap(lambda x: x.split(':')[0]).astype(int).to_numpy()
+    # D = df.copy().applymap(lambda x: x.split(':')[1]).astype(float).to_numpy()
+    I = df.copy().applymap(lambda x: x.split(':')[0]).to_numpy()
+    D = df.copy().applymap(lambda x: x.split(':')[1]).to_numpy()
+    store_results(result_dst, algo, kind, D, I, buildtime, querytime, params, size)
     print(f'.h5 result file successfully created: {result_dst}')
 
 if __name__ == "__main__":
