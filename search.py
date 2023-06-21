1a5730dbf06f03a45746103b830bd2336a9e1686
xsedmid <sedmidubsky@gmail.com>
2023-06-21 14:17:57 +0200

    test - input params

--- search.py
+++ search.py
@@ -57,6 +57,7 @@ def run(root_data_folder, kind, key, size="100K", k=30, buildindex=True):
     # Run Java-based implementation (building the index + searching)...
     print(f"running Java-based implementation (building the index + searching)...")
     dataset_size_dict = {"100K": 100000, "300K": 300000, "10M": 10000000, "30M": 30000000, "100M": 100000000}
+    print(f'* params: {dataset_orig_path}, {dataset_path}, {query_orig_path}, {query_path}, {dataset_size_dict[size]}, {buildindex}, {k}')
     start_time = time.time()
     subprocess.check_output(['java', '-Xmx256g', '-jar', os.path.join(os.getcwd(), 'VMTrials', 'target', 'VMTrials-1.0-SNAPSHOT-jar-with-dependencies.jar'), dataset_orig_path, dataset_path, query_orig_path, query_path, dataset_size_dict[size], buildindex, k], universal_newlines=True)
     print(f"done in {time.time() - start_time}s")
@@ -67,7 +68,7 @@ def run(root_data_folder, kind, key, size="100K", k=30, buildindex=True):
         output_params = f.read()
         output_params = dict(item.split(":") for item in output_params.strip().split(";"))
         print(f"output_params: {output_params}")
-    algo = 'SimRELv1'
+    algo = 'CRANBERRY'
     buildtime = output_params['buildtime']
     querytime = output_params['querytime']
     if 'params' in output_params:
